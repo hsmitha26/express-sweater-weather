3efc001d0694f423399a63b5965418929077b431
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-21 14:17:14 -0700

    Refactors finding and verifying user.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -8,13 +8,12 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
 router.get('/', (request, response) =>  {
-  database('users').first()
+  let userCredential = request.body.api_key
+  let location = request.query.location
+  let geocode = process.env.GEOCODE_API_KEY
+
+  database('users').where({apiKey: userCredential}).first()
     .then(user => {
-      let userCredential = request.body.api_key
-
-      let location = request.query.location
-      let geocode = process.env.GEOCODE_API_KEY
-
       if (userCredential === user.apiKey) {
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${geocode}`)
           .then(response => response.json())
@@ -22,6 +21,7 @@ router.get('/', (request, response) =>  {
             let latitude = result.results[0].geometry.location.lat
             let longitude = result.results[0].geometry.location.lng
             let darkSky = process.env.DARKSKY_API_KEY
+            
             fetch(`https://api.darksky.net/forecast/${darkSky}/${latitude},${longitude}`)
               .then(response => response.json())
               .then(result => {
