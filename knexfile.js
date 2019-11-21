032541e6ec165a6109ae7fb07b79079811389c1e
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-20 19:34:55 -0700

    configures production environment

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'https://express-weather-forecast.herokuapp.com/',
+    connection: 'postgres://smsxwjxsddxzqg:41fed8edb25f542694f5b75118ab5e610cd8c48a7628cc681707275fdcd9aec4@ec2-54-225-195-3.compute-1.amazonaws.com:5432/ddr1feuitfd1ed',
     migrations: {
       directory: './db/migrations'
     },
