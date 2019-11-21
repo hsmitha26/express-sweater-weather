92b90b0a3bc0da8180ca38a25d9a6470811240e2
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-20 17:45:21 -0700

    generates forecast data, needs to be parsed.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,14 +1,41 @@
+var _env = require('dotenv').config();
 var express = require('express');
 var router = express.Router();
-
-require('dotenv').config()
-const geocode = process.env.GEOCODE_API_KEY
-const darkSky = process.env.DARKSKY_API_KEY
-
 const fetch = require('node-fetch');
 
-function invitesIndex(){
-  fetch("https://hedgehog-party.herokuapp.com/api/v1/invites")
-  .then(response => response.json())
-  .then(result => console.log(result))
-};
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
+
+router.get('/', (request, response) =>  {
+  database('users').first()
+    .then(user => {
+      let userCredential = request.body.api_key
+
+      let location = request.query.location
+      let geocode = process.env.GEOCODE_API_KEY
+
+      if (userCredential === user.apiKey) {
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${geocode}`)
+          .then(response => response.json())
+          .then(result => {
+            let latitude = result.results[0].geometry.location.lat
+            let longitude = result.results[0].geometry.location.lng
+            let darkSky = process.env.DARKSKY_API_KEY
+            fetch(`https://api.darksky.net/forecast/${darkSky}/${latitude},${longitude}`)
+              .then(response => response.json())
+              .then(result => {
+                console.log(result)
+                // var currentlyForecast = {parse result} ,
+                // var hourlyForecast = {parse result}
+                // var data = {currently: currentlyForecast, hourly: hourlyForecast}
+                // response.status(200).send(data)
+              })
+            })
+          } else {
+            response.status(401).send("Unauthorized")
+          }
+    })
+  });
+
+module.exports = router;
