0bea9dcd3f2df1b2ff47d82e712e0cc039702b7f
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-20 21:35:53 -0700

    adds logic to verify user's api key. 401 response successful

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
+
 // pass in JSON with location and api_key into url
 
 router.post('/', (request, response) => {
-  console.log(request.body, "got it!")
+  database('users').first()
+    .then(user => {
+      let userCredential = request.body.api_key
+      let location = request.body.location
+
+      if (userCredential === user.apiKey) {
+        console.log(userCredential, location, "got it!")
+        // add location to user's locations
+        // data = {
+                  //   "message": "Denver, CO has been added to your favorites",
+                  // }
+        // response.status(200).send(data)
+      } else {
+        response.sendStatus(401)
+      }
+
+    })
 });
 
 module.exports = router;
