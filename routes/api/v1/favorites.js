a995b0017f1870cfd8014f98a1d3284be2b17929
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-11-20 22:28:48 -0700

    successfully renders JSON response.

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -5,8 +5,6 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
-// pass in JSON with location and api_key into url
-
 router.post('/', (request, response) => {
   database('users').first()
     .then(user => {
@@ -14,16 +12,17 @@ router.post('/', (request, response) => {
       let location = request.body.location
 
       if (userCredential === user.apiKey) {
-        console.log(userCredential, location, "got it!")
-        // add location to user's locations
-        // data = {
-                  //   "message": "Denver, CO has been added to your favorites",
-                  // }
-        // response.status(200).send(data)
+        database('users').where('apiKey', userCredential)
+          .then(user => {
+            database('locations').insert({name: location, user_id: user})
+            var data = {
+                          "message": `${location} has been added to your favorites`
+                        }
+            response.status(200).send(data)
+          })
       } else {
         response.sendStatus(401)
       }
-
     })
 });
 
